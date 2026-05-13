7fdc4037a01f8a55674c28773c00529bd1a69a6a
Xenofon Kousidis <xenkou90@gmail.com>
2026-05-13 16:01:16 +0300

    feat: prevent rubber-band scroll on home page for app-like feel

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -16,6 +16,7 @@ export default function Home() {
         p-6
         font-[family-name:var(--font-mono)]
         relative
+        overflow-hidden
       "
     >
       {/* Info button - top right corner */}
